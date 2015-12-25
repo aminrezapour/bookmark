deb789df43061cd37c1698ef765bc3d71680a43a
Amin Rezapour <aminrezapour@gmail.com>
2015-12-24 18:02:50 -0800

    Add incoming mail controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,14 +3,23 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
 
-    # You put the message-splitting and business
-    # magic here.
+   # You put the message-splitting and business
+   # magic here.
+    @user = User.find_by_email(params[:sender])
 
-    # Assuming all went well.
-    head 200
+    # What happens to confirmation here?
+    if @user.nil?
+      @user = User.create!(email: params[:sender], password: "guest1234")
+    end
+
+    @topic = @user.topics.find_or_create_by(title: params[:subject])
+
+    link_url_string = params["body-plain"]
+
+    @topic.links.create!(url: link_url_string)
+
+   # Assuming all went well.
+   head 200
   end
 end
